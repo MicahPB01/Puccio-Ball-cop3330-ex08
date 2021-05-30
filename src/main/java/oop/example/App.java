582fe1a50a7582ebc97f497b2137370821930913
MicahPB01 <micahpuccioball@gmail.com>
2021-05-30 12:50:48 -0400

    Initial commit

---
+++ src/main/java/oop/example/App.java
@@ -0,0 +1,29 @@
+package oop.example;
+
+import java.util.Scanner;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int numPeople;
+        int numPizza;
+        int slicesPer;
+        int leftOver;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.printf("How many people? ");
+        numPeople = Integer.parseInt(input.nextLine());
+
+        System.out.printf("How many pizzas do you have? ");
+        numPizza = Integer.parseInt(input.nextLine());
+
+        System.out.printf("How many slices per pizza? ");
+        slicesPer = Integer.parseInt(input.nextLine());
+
+        System.out.printf("%d people with %d pizzas (%d slices)\n", numPeople, numPizza, numPizza*slicesPer);
+        System.out.printf("Each person gets %d slices of pizza.\n", ((numPizza*slicesPer)/numPeople));
+        System.out.printf("There are %d leftover pieces.\n", ((numPizza*slicesPer) % numPeople));
+
+    }
+}
